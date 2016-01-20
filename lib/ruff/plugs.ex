341749bef40399c5e78333fc57458cef498f1eeb
TurplePurtle <santi256@gmail.com>
2016-01-20 10:40:19 -0600

    use Maybe.map_nil

--- lib/ruff/plugs.ex
+++ lib/ruff/plugs.ex
@@ -2,13 +2,10 @@ defmodule Ruff.Plugs do
   import Plug.Conn
   alias Ruff.Repo
   alias Ruff.User
-  alias Monocle.Maybe
+  import Monocle.Maybe, only: [map_nil: 2]
 
   def current_user(conn, _) do
-    user =
-      Maybe.from_nil(get_session(conn, :current_user))
-      |> Maybe.map(&Repo.get(User, &1))
-      |> Maybe.get()
+    user = get_session(conn, :current_user) |> map_nil(&Repo.get(User, &1))
     assign(conn, :current_user, user)
   end
 end
